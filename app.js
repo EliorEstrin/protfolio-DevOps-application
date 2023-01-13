9af0fe49c36fa578749783ac5e18ad18788306cc
elior <elior1001@gmail.com>
2023-01-14 01:34:40 +0200

    added delete task form in ui

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const option_update_btn = document.querySelector("#option_update");
 
 //Sumbit btn
 const submit_create_btn = document.querySelector('#create_task_btn');
-
+const submit_delete_btn = document.querySelector('#delete_task_btn');
 
 
 // by deafult:
@@ -19,10 +19,12 @@ hideAll()
 // Usefull when the page reload after a form get submitted
 function hideAll(){
     hideForm('#create-task-form')
+    hideForm('#delete-task-form')
 }
 
 // gets an element and shows him on the page
 function showForm(element_id) {
+    hideAll()
     var element = document.querySelector(element_id);
     element.style.display = 'inline-block';
 
@@ -34,10 +36,13 @@ function hideForm(element_id) {
     element.style.display = 'none';
 }
 
+
 // On Create btn click
 option_create_btn.addEventListener('click', () => showForm('#create-task-form'));
-
-
 // After submitted form can be removed
 submit_create_btn.addEventListener('click', () => hideForm('#create-task-form'));
 
+// On delete btn click
+option_delete_btn.addEventListener('click', () => showForm('#delete-task-form'))
+// After submitted form can be removed
+submit_delete_btn.addEventListener('click', () => hideForm('#delete-task-form'))
