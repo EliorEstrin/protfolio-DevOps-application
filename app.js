86fe0ba942587e8671431358306a1b75ad394f05
elior <elior1001@gmail.com>
2023-01-14 00:40:50 +0200

    id for submit btn change in js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // Definitation of btn that acced later in the code
+//Options
 const option_create_btn = document.querySelector("#option_create");
-const submit_create_btn = document.querySelector('#create_task_button');
+const option_delete_btn = document.querySelector("#option_delete");
+const option_edit_btn = document.querySelector("#option_edit");
+const option_update_btn = document.querySelector("#option_update");
+
+//Sumbit btn
+const submit_create_btn = document.querySelector('#create_task_btn');
+
+
 
 // by deafult:
 hideAll()
@@ -10,7 +18,7 @@ hideAll()
 // Funtion to hide all form elements that created afer a btn is pressed
 // Usefull when the page reload after a form get submitted
 function hideAll(){
-    hideCreateTask()
+    hideForm('#create-task-form')
 }
 
 // gets an element and shows him on the page
@@ -20,23 +28,16 @@ function showForm(element_id) {
 
 }
 
+// Gets an elemnt id and removes it from the page
+function hideForm(element_id) {
+    var element = document.querySelector(element_id);
+    element.style.display = 'none';
+}
 
-
-// option_create_btn.addEventListener('click', showCreateTask)
+// On Create btn click
 option_create_btn.addEventListener('click', () => showForm('#create-task-form'));
 
 
 // After submitted form can be removed
-submit_create_btn.addEventListener('click', hideCreateTask);
+submit_create_btn.addEventListener('click', () => hideForm('#create-task-form'));
 
-
-
-function hideCreateTask() {
-    var createTask = document.querySelector('#create-task-form');
-    createTask.style.display = 'none';
-}
-
-function showCreateTask() {
-var createTask = document.querySelector('#create-task-form');
-createTask.style.display = 'inline-block';
-}
