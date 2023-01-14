43d052a329a48da08f5073c2e360c253e5e530e0
elior <elior1001@gmail.com>
2023-01-14 21:05:15 +0200

    main folder clened

--- application/mongo.py
+++ application/mongo.py
@@ -6,12 +6,45 @@ def connection():
     client = MongoClient("mongodb://root:example@localhost:27017")
     # Creating a data base
     db = client['tasks']
-
     return db
-# db.create_collection('myCollection')
 
 
-connection()
+def get_Tasks():
+    db = connection()
+    task_collection = db["task_collection"]
+    tasks = task_collection.find()
+    return tasks
+
+
+
+def add_sample_data():
+    db = connection()
+    task_collection = db["task_collection"]
+    sample_data = [{
+        "taskName": "Task 1",
+        "description": "This is the first task",
+        "assignedTo": "John Doe",
+        "status": "Not Started"
+    }, {
+        "taskName": "Task 2",
+        "description": "This is the second task",
+        "assignedTo": "Jane Smith",
+        "status": "In Progress"
+    }]
+    task_collection.insert_many(sample_data)
+
+# add_sample_data()
+
+
+
+
+# all_task = get_Tasks()
+# for task in all_task:
+#     print(task)
+
+
+
+
 
 
 #isting all the databases
